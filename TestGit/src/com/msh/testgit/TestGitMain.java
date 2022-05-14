c90588c1b45a564aaad822109361723f32e53509
Showkat <Showkat@DESKTOP-EKU2SID.attlocal.net>
2022-05-14 11:33:41 -0500

    Test Git first commit

---
+++ TestGit/src/com/msh/testgit/TestGitMain.java
@@ -0,0 +1,12 @@
+package com.msh.testgit;
+
+public class TestGitMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is git test");
+
+	}
+
+}
